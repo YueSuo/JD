e5a0154cc8caacd6a4ec5de72fe19b526245351e
YueSuo <923821545@qq.com>
2018-04-09 23:38:52 +0800

    完善中间布局，地址，已选，评价晒单等

--- JD/app/src/main/java/com/example/sy/jd/JdActivity.java
+++ JD/app/src/main/java/com/example/sy/jd/JdActivity.java
@@ -1,7 +1,13 @@
 package com.example.sy.jd;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class JdActivity extends AppCompatActivity {
 
@@ -9,5 +15,6 @@ public class JdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jd);
+
     }
 }
